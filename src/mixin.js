6fb91aa82a2282a3f851924fce13eeabfd95c425
Metin Seylan <metnsyln@gmail.com>
2016-10-30 18:30:21 +0300

    sıcak tamir

--- src/mixin.js
+++ src/mixin.js
@@ -4,7 +4,7 @@ import Main from './main';
 export default {
         install: function (Vue) {
             Vue.mixin({
-                init: function () {
+                beforeCreate: function () {
                     this.$form = (new Main(this.$http)).form;
                 }
             });
